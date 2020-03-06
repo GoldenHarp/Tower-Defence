9e61deb910053fce8035dab14a275e4ba4dd27ab
GoldenHarp <55559896+GoldenHarp@users.noreply.github.com>
2020-03-06 17:08:28 -0600

    ye

--- Main_Game_File.py
+++ Main_Game_File.py
@@ -8,6 +8,41 @@ pygame.init()
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
-
 game_window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Tower Defence")
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+clock = pygame.time.Clock()
+
+
+
+
+
+def main_game_loop():
+
+    game_loop = True
+
+    while game_loop:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                game_loop = False
+
+        pygame.display.update()
+        clock.tick(60)
+    pygame.quit()
+   
+    
+
+
+
+main_game_loop()
+
+
+
+
+    
